3fe90cf0d82e5796695553b90a055063827726cd
mike verdone <mverdone@github.com>
2017-02-22 22:57:04 -0500

    Added functions to support operators (incomplete)

---
+++ Source/support.js
@@ -0,0 +1,56 @@
+// for functions that reference stack 'top' and 'top minus one': op1 = top, op2 = top -1
+
+function add(op1,op2)
+{
+	var sum = op1 + op2;
+	return sum;
+};
+
+function subtract(op1,op2)
+{
+	var difference = op1 - op2;
+	return difference;
+};
+
+function mult(op1,op2)
+{
+	var product = op1 * op2;
+	return product;
+}
+
+function divide(op1,op2)
+{
+	var quotient = op1 / op2;
+	return Math.floor(quotient);
+}
+
+function div-mod(op1,op2)
+{
+	var remainder = op1 % op2;
+	return remainder;
+}
+
+function logic_AND(op1,op2)
+{
+	
+}
+
+function logic_OR(op1,op2)
+{
+	
+}
+
+function negate(op1)
+{
+	
+}
+
+function check_equality(op1,op2)
+{
+	
+}
+
+function less_or_equal(op1,op2)
+{
+	
+}
