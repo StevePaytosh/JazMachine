9f42de76447d17d0070d5569ad8d7f61e53931ad
Steve-Paytosh <mrblond619f5@hotmail.com>
2017-02-22 19:23:40 -0500

    stack operations will handle scope changes

--- Source/stack.js
+++ Source/stack.js
@@ -1,28 +1,39 @@
-var data=[]; //general stack for numbers
 var vars=[]; //stack for variables, index of type string: vars["f"]=0 , vars["ff"]=15
+var d=[];
+var v=[];
 var current_var; //pointer(string index) to the current var
 var labels=[]; //holds strings that act as sections of code identified by labels
 var routines=[]; //holds strings that acts sections of code that act as routines (consider using an object)
+var data_scope=0;
+var vars_scope=0;
 
+
+function init()
+{
+	d.push([]);
+	v.push([]);
+}
 function lvalue(newVar)
 {
 	current_var=newVar;
+	v[vars_scope][newVar]="mu";
+	
 };
 
 function  rvalue(val)
 {
-	data.push(val);
+	d[data_scope].push(val);
 };
 
 function  set()
 {
-	vars[current_var]=data.pop();
-	console.log(current_var + " equals "+vars[current_var]);
+	v[vars_scope][current_var]=pop();
+	console.log(current_var + " equals "+v[vars_scope][current_var]);
 };
 
 function getVar(name)
 {
-	return vars[name];
+	return v[vars_scope][name];
 }
 function  setLabel(name,code)
 {
@@ -31,21 +42,56 @@ function  setLabel(name,code)
 
 function  pop()
 {
-	var r=data.pop();
+	var r=d[data_scope].pop();
 	console.log("poped: " + r + " from the stack\n");
+	return r;
 }
 
 function push(val)
 {
-		data.push(val);
+	d[data_scope].push(val);
 }
 
 function  topOfStack()
 {
-	return data[0];
+	return d[data_scope][d[data_scope].length-1];
 }
 
 function  call_goto(name)
 {
 	return labels[name];
+}
+
+function setScope(val)
+{
+	val=val.toLowerCase()
+	if(val=="begin")
+	{
+		vars_scope++;
+		v.push([]);
+	}
+	
+	if(val=="call")
+	{
+		data_scope++;
+		d.push([]);
+	}
+	
+	if(val=="return")
+	{
+		vars_scope--;
+		v.pop();
+	}
+	
+	if(val=="end")
+	{
+		data_scope--;
+		d.pop();
+	}
+	
+	if(data_scope<0) //prevent the scopes from going negative
+		data_scope=0;
+	
+	if(vars_scope<0)
+		data_scope=0;
 }
--- Source/tests.js
+++ Source/tests.js
@@ -14,14 +14,28 @@ function stackTests()
 	rvalue(1);
 	rvalue(2);
 	
-	setScope("begin"); // lvaues are not inner, rvalues are now outer
+	setScope("begin"); // lvaues are now inner, rvalues are outer
 	
 	rvalue(11);
 	lvalue("ff");
 	set(); //ff=11
 	
-	setScope("end");
+	setScope("call"); //l&r values are both inner
+	lvalue("lp");
+	lvalue("zz");
+	rvalue(12);
+	rvalue(13);
+	set(); //zz=13
+	
+	setScope("return"); //lvalue outer, rvalue inner
+	lvalue("lp1");
+	set(); // lpl=12
+	
+	setScope("end"); //outer, outer
 	set(); //x=2
+	
+	set(); //x=2
+	rvalue(99);
 	pop(); //pop 1
 	
 }
