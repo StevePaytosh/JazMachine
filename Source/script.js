371cf0deabd493016552933c8e4445b3da707d79
Steve-Paytosh <mrblond619f5@hotmail.com>
2017-02-23 13:57:08 -0500

    added method to output for print and show

--- Source/script.js
+++ Source/script.js
@@ -15,10 +15,7 @@ window.onload = function() {
 
 				reader.onload = function(e) {
 					//fileDisplayArea.innerText = reader.result;
-					//fileDisplayArea.innerText=reader.result.split('\n')[0];
-					var doc=reader.result.split('\n');
-					init(doc);
-					run_file(doc,0,doc.length);
+					fileDisplayArea.innerText=reader.result.split('\n')[0];
 				}
 
 				reader.readAsText(file);	
@@ -28,76 +25,21 @@ window.onload = function() {
 		});
 };
 
-function run_file(doc,start, end)
+function print()
 {
-	//this function gets the array representing the file and iterated through the lines
-	for(var i=start; i<end; i++)
-	{
-		doc[i]=doc[i].trim();
-		var args=doc[i].split(" "); //split up the line on spaces
-		args = args.map(item => { return item.trim() })
-		//var params=args.join("",2,args.length-1); //conjoin the remainder of the array to create the parameter
-		var params=args.splice(1,args.length).join(" ");
-		var opperation=args[0]; //the first element in the args array is the function to call
-		
-		if(opperation.includes("goto"))
-		{
-			//should not return
-			var s=getLabel(params);
-			run_file(doc,s,end);
-			return 0;
-		}
-		
-		call(opperation,params);
-		
-		if(opperation.includes("call"))
-		{
-			//should return after call
-			var s=getLabel(params);
-			run_file(doc,s,end);
-		}
-		else if(opperation.includes("return") )
-		{
-			return;
-		}
-	}
-	
+	//gets the top of the stack and prints italics
+	var top=top();
+	var element=$("#out");
+	element.innerHTML=element.innerHTML+cont;
+}
+
+function show(cont)
+{
+	//gets the top of the stack and prints italics
+	var top=top();
+	var element=$("#out");
+	element.innerHTML=element.innerHTML+cont+"\n";
 	
 }
 
-function call(opp,param)
-{
-	switch(opp)
-	{
-		case "lvalue": lvalue(param);break;
-		case "rvalue": rvalue(param);break;
-		case ":=": set(); break;
-		case "push": push(param); break;
-		case "pop": pop(); break;
-		case "call":
-		case "return":
-		case "end":	
-		case "begin": setScope(opp); break;
-		case "+": add(pop(),pop()); break;
-		case "-": subtract(pop(),pop()); break;
-		case "*": mult(pop(),pop()); break;
-		case "/": divide(pop(),pop()); break;
-		case "div": div-mod(pop(),pop()); break;
-		case "&": logic_AND(pop(),pop());
-		case "!": negate(pop());
-		case "|": logic_OR(pop(),pop());
-		default:
-		{
-			if(opp.includes(":="))
-				set();
-			else if(opp.includes("push"))
-				push(param);
-			else if(opp.includes("pop"))
-				pop();
-			else if(opp.includes("call") || opp.includes("return") || opp.includes("end")  || opp.includes("begin"))
-				setScope(param);
-		}
-	
-	}
-	
-}
+
