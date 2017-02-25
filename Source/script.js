9f183f6117a388ec7e05be244243865edefa1f2d
Steve-Paytosh <mrblond619f5@hotmail.com>
2017-02-25 13:24:39 -0500

    reset

--- Source/script.js
+++ Source/script.js
@@ -1,45 +1,5 @@
 $(document).ready(function(){
 });
 
-window.onload = function() {
-		var fileInput = document.getElementById('fileInput');
-		var fileDisplayArea = document.getElementById('out');
-
-		fileInput.addEventListener('change', function(e) {
-			var file = fileInput.files[0];
-			//var textType = /text.*/;
-			var textType = /.*/
-
-			if (file.type.match(textType)) {
-				var reader = new FileReader();
-
-				reader.onload = function(e) {
-					//fileDisplayArea.innerText = reader.result;
-					fileDisplayArea.innerText=reader.result.split('\n')[0];
-				}
-
-				reader.readAsText(file);	
-			} else {
-				fileDisplayArea.innerText = "File not supported!"
-			}
-		});
-};
-
-function print()
-{
-	//gets the top of the stack and prints italics
-	var top=top();
-	var element=$("#out");
-	element.innerHTML=element.innerHTML+cont;
-}
-
-function show(cont)
-{
-	//gets the top of the stack and prints italics
-	var top=top();
-	var element=$("#out");
-	element.innerHTML=element.innerHTML+cont+"\n";
-	
-}
 
 
