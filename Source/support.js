1d37e7e99fb2f2c9a6ca071119748e2fd9f61edb
Steve-Paytosh <mrblond619f5@hotmail.com>
2017-02-26 23:18:24 -0500

    updated methods in support

--- Source/support.js
+++ Source/support.js
@@ -2,58 +2,55 @@
 
 function add(op1,op2)
 {
-	//var sum = 
 	return +op1 + +op2;
-	//return sum;
 };
 
 function subtract(op1,op2)
 {
-	//var difference = 
-	return op1 - op2;
-	//return difference;
+	return +op1 - +op2;
 };
 
 function mult(op1,op2)
 {
-	//var product = 
-	return op1 * op2;
-	//return product;
+	return +op1 * +op2;
 }
 
 function divide(op1,op2)
 {
-	//var quotient = 
-	return op1 / op2;
-	//return Math.floor(quotient);
+	return +op1 / +op2;
 }
 
 function div_mod(op1,op2)
 {
-	//var remainder = 
-	return op1 % op2;
-	//return remainder;
+	return +op1 % +op2;
 }
 
 function logic_AND(op1,op2)
 {
-	return op1 && op2;
+	return +op1 && +op2;
 }
 
 function logic_OR(op1,op2)
 {
-	return op1 || op2;
+	return +op1 || +op2;
 }
 
 function negate(op1)
 {
-	return !op1;
+	return !(+op1);
 }
 
 function check_equality(op1,op2)
 {
-	if (op1 == op2) return 0;
-	else return 1;
+	//<>
+	return !(+op1 == +op2);
+}
+
+function check_diff(op1,op2)
+{
+	// =
+		if((+op1-+op2)==0)
+			return 0;
 }
 
 function less_or_equal(op1,op2)
